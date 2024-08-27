567858c551331fa2a77d2a883b99267bcfdb2ae1
TomD <T.Demeyere@soton.ac.uk>
2024-08-27 13:29:04 +0100

    fix

--- source/_static/launch_onetep.sh
+++ source/_static/launch_onetep.sh
@@ -12,16 +12,16 @@ nthreads=2
 
 # Point this to your ONETEP executable 
 # (normally found in your installation's ./bin directory).
-onetep_exe=/home/user/onetep/bin/onetep.gfortran
+onetep_exe=/local/disk2/tom_stuff_please_do_not_delete/onetep/bin/onetep.RH8.ifort.omp.scalapack
 
 # Point this to your onetep_launcher
 # (normally found in your installation's ./utils directory).
-onetep_launcher=/home/user/onetep/utils/onetep_launcher
+onetep_launcher=/local/disk2/tom_stuff_please_do_not_delete/onetep/utils/onetep_launcher
 
 # Load any necessary modules here, set up any necessary environment variables.
 # e.g. for Intel you might have to
-# module load oneapi
-# source /local/software/intel/oneapi/2023.0/setvars.sh
+module load oneapi
+source /local/software/intel/oneapi/2023.0/setvars.sh
 
 # This runs ONETEP. On some systems you may need to replace 'mpirun'
 # with a suitable alternative.
